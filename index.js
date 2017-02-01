25df30ef3fd577183cb553896355a832595f747e
Mohammed Chisti <Chisti@Mohammeds-MacBook-Air.local>
2017-02-01 16:41:28 -0500

    Done.

--- index.js
+++ index.js
@@ -13,3 +13,29 @@ ReactDOM.render(
   ]),
   document.getElementById('main')
 );
+
+class OlderCoaster extends React.Component {
+  render() {
+    return React.createElement( 'div', { className: 'oldercoster' }, '')
+  }
+}
+
+class InFrontOfYou extends React.Component {
+  render() {
+    strong = React.createElement( 'strong', {}, 'right in front of you.' )
+    p = React.createElement( 'p', {}, ['Sometimes the solution', strong])
+    p1 = React.createElement( 'p', {}, 'Sometimes the solution is ')
+
+    return React.createElement( 'div', { }, [p1, p])
+  }
+}
+
+class ButcherShop extends React.Component {
+  render() {
+    strong = React.createElement( 'strong', {}, 'right in front of you.' )
+    p = React.createElement( 'p', {}, ['Sometimes the solution', strong])
+    p1 = React.createElement( 'p', {}, 'Sometimes the solution is ')
+
+    return React.createElement( 'div', { }, [p1, p])
+  }
+}
